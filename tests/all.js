595f02065737633e1c88a3b80e304997f97036f6
Bogdan BIVOLARU <bogdan.bivolaru@gmail.com>
2014-04-07 01:53:10 +0300

    ﻿Initial empty intern + dojo project

---
+++ tests/all.js
@@ -0,0 +1,5 @@
+define([
+	'./model/SimpleTodoModel', // 2 tests fail
+	// 'intern/node_modules/dojo/has!host-browser?./store/LocalStorage',
+	// 'intern/node_modules/dojo/has!host-browser?./form/CheckBox'
+], function () {});
