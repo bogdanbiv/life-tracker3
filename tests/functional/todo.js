61b98fd1f9899be284a5a776281270d6f056df0d
Bogdan Bivolaru <bogdan.bivolaru@gmail.com>
2014-06-21 13:05:30 +0300

    Tests for DojoX Calendar

---
+++ tests/functional/todo.js
@@ -0,0 +1,42 @@
+﻿ 
+define([
+    'intern!object',
+    'intern/chai!assert',
+    'require',
+], function (registerSuite, assert, require) {
+    /*assert.strictEqual(registerSuite, {});
+    registerSuite({
+        name: 'index',
+        '#greeting form': function () {
+          // assert.strictEqual(this, {});
+          console.log(JSON.stringify(this));
+        },
+    }); //// */
+
+    registerSuite({
+        name: 'index',
+
+        '#calendar_details': function () {
+            return this.remote
+                .get(require.toUrl('public/demos/calendar/index.html'))
+                //.waitForElementByCssSelector(".dijitAccordionContainer .dijitAccordionText", 1200)
+                .elementById('dijit_layout_ContentPane_0_button_title').text()
+                .then(function (text) {
+                  assert.strictEqual(text, 'Main properties', 'simple functional test');
+                });
+
+                /*.elementById('nameField')
+                    .clickElement()
+                    .type('Elaine2')
+                    .end()
+                .elementByCssSelector('#loginForm input[type=submit]')
+                    .clickElement()
+                    .end()
+                .elementById('greeting')
+                .text()
+                .then(function (text) {
+                    assert.strictEqual(text, 'Hello, Elaine!', 'Greeting should be displayed when the form is submitted');
+                });*/
+        }
+    });/// */;
+});
---
+++ tests/model/SimpleTodoModel.js
@@ -0,0 +1,40 @@
+﻿// define([
+//     'intern!object',
+//     'intern/chai!assert',
+//     'app/hello'
+// ], function (registerSuite, assert, hello) {
+//   registerSuite({
+//         name: 'hello',
+//
+//         greet: function () {
+//             assert.strictEqual(hello.greet('Murray'), 'Hello, Murray!', 'hello.greet should return a greeting for the person named in the first argument');
+//             assert.strictEqual(hello.greet(), 'Hello, world!', 'hello.greet with no arguments should return a greeting to "world"');
+//         }
+//     });
+// });
+define([
+  'intern!object',
+  'intern/chai!assert',
+  "dojo/ready",
+  "dojo/_base/lang",
+  "dojo/_base/fx",
+  "dojo/dom",
+  "demos/calendar/utils",
+  //"demos/calendar/ExtendedCalendar"
+], function(
+  registerSuite,
+  assert,
+  ready,
+  lang,
+  fx,
+  dom,
+  utils
+) {
+    registerSuite({
+        name: 'createDefaultStore',
+
+        createDefaultStore: function () {
+            calInstance = utils.createDefaultStore({});
+        }
+    });
+});
