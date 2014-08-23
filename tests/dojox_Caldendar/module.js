c0124066b48da06b6bead4460dd89e7470ab923e
Bogdan Bivolaru <bogdan.bivolaru@gmail.com>
2014-08-23 21:26:09 +0300

    Remove old test directory
    Signed-off-by: Bogdan Bivolaru <bogdan.bivolaru@gmail.com>

--- tests/dojox_Caldendar/module.js
+++
@@ -1,9 +0,0 @@
-﻿define([
-    "tests/dojox_Caldendar/time",
-    "tests/dojox_Caldendar/itemEditing",
-    "tests/dojox_Caldendar/renderingProjection",
-    "tests/dojox_Caldendar/timeRanges",
-    "tests/dojox_Caldendar/store",
-
-], 1);
-
